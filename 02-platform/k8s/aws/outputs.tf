9a04c72e54b94ca296694de4b1a99e2a01617945
Tony Phan <tphan@hashicorp.com>
2025-03-11 17:59:57 +1100

    Add output for EKS cluster API endpoint in outputs.tf

--- 02-platform/k8s/aws/outputs.tf
+++ 02-platform/k8s/aws/outputs.tf
@@ -2,3 +2,8 @@ output "cluster_name" {
   description = "eks cluster name"
   value       = module.eks.cluster_name
 }
+
+output "cluster_api_endpoint" {
+  description = "eks cluster api endpoint"
+  value       = module.eks.cluster_endpoint
+}
