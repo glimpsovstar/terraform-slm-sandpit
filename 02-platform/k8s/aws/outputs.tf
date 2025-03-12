2e2af088d5483a7dc20d69473c0a77c643de9552
Tony Phan <tphan@hashicorp.com>
2025-03-12 12:59:40 +1100

    Add CA certificate output for EKS cluster in outputs.tf

--- 02-platform/k8s/aws/outputs.tf
+++ 02-platform/k8s/aws/outputs.tf
@@ -7,3 +7,8 @@ output "cluster_api_endpoint" {
   description = "eks cluster api endpoint"
   value       = module.eks.cluster_endpoint
 }
+
+output "cluster_ca_certificate" {
+  description = "eks cluster ca certificate"
+  value       = module.eks.cluster_certificate_authority_data
+}
