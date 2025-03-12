40db0975ea47c46c562ee0927108139d55f6fd93
Tony Phan <tphan@hashicorp.com>
2025-03-13 09:41:17 +1100

    Add OIDC issuer URL output for EKS cluster in outputs.tf

--- 02-platform/k8s/aws/outputs.tf
+++ 02-platform/k8s/aws/outputs.tf
@@ -12,3 +12,9 @@ output "cluster_ca_certificate" {
   description = "eks cluster ca certificate"
   value       = module.eks.cluster_certificate_authority_data
 }
+
+output "cluster_oidc_issuer_url" {
+  description = "eks cluster oidc issuer"
+  value       = module.eks.cluster_oidc_issuer_url
+  
+}
