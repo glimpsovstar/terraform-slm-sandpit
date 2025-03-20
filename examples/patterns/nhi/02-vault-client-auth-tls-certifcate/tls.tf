a6ad03b52c160a9fab57ea47da1221410a618e1b
Tony Phan <tphan@hashicorp.com>
2025-03-20 20:01:24 +1100

    Add TLS certificate resource and variables for Vault client authentication

---
+++ examples/patterns/nhi/02-vault-client-auth-tls-certifcate/tls.tf
@@ -0,0 +1,5 @@
+resource "vault_pki_secret_backend_cert" "client-auth-tls" {
+  backend     = var.pki_mount_path
+  name        = var.pki_role
+  common_name = var.common_name
+}
---
+++ examples/patterns/nhi/02-vault-client-auth-tls-certifcate/variables.tf
@@ -0,0 +1,14 @@
+variable "pki_mount_path" {
+  description = "pki mount path"
+  type        = string
+}
+
+variable "common_name" {
+  description = "common name"
+  type        = string
+}
+
+variable "pki_role" {
+  description = "pki role"
+  type        = string
+}
