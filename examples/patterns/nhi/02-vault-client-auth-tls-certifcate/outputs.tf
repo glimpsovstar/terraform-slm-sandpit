90969b2f904a529b62dd33103cee38f6f17d68c8
Tony Phan <tphan@hashicorp.com>
2025-03-21 07:40:44 +1100

    Rename output variables for client authentication certificate and private key for clarity

--- examples/patterns/nhi/02-vault-client-auth-tls-certifcate/outputs.tf
+++ examples/patterns/nhi/02-vault-client-auth-tls-certifcate/outputs.tf
@@ -1,7 +1,7 @@
-output "client_auth_certificate" {
+output "public_cert_pem" {
   value = vault_pki_secret_backend_cert.client-auth.certificate
 }
 
-output "client_auth_private_key" {
+output "private_key_pem" {
   value = vault_pki_secret_backend_cert.client-auth.private_key
 }
