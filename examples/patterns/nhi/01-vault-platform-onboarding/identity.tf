d5a51bd652750170e0c52e6aa269c15e4f681451
Tony Phan <tphan@hashicorp.com>
2025-05-22 15:43:28 +1000

    Refactor identity entity alias resource for test user in Vault configuration

--- examples/patterns/nhi/01-vault-platform-onboarding/identity.tf
+++ examples/patterns/nhi/01-vault-platform-onboarding/identity.tf
@@ -6,15 +6,15 @@ resource "vault_identity_entity" "testuser1" {
   }
 }
 
+resource "vault_identity_entity_alias" "testuser1" {
+  name            = "testuser1"
+  mount_accessor  = vault_auth_backend.userpass.accessor
+  canonical_id    = vault_identity_entity.testuser1.id
+}
+
 resource "vault_identity_group" "testing" {
   name     = "testing"
   member_entity_ids = [
     vault_identity_entity.testuser1.id
   ]
-}
-
-resource "vault_identity_entity_alias" "testuser1" {
-  name            = "testuser1"
-  mount_accessor  = vault_auth_backend.userpass.accessor
-  canonical_id    = vault_identity_entity.testuser1.id
 }
