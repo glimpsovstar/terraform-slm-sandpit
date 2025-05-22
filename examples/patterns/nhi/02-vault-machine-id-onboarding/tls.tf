e6cfbf64f8d394b3e494d68acbba2b90bb2a43be
Tony Phan <tphan@hashicorp.com>
2025-05-22 13:32:30 +1000

    Add ip_sans configuration for machine identity certificate

--- examples/patterns/nhi/02-vault-machine-id-onboarding/tls.tf
+++ examples/patterns/nhi/02-vault-machine-id-onboarding/tls.tf
@@ -3,4 +3,7 @@ resource "vault_pki_secret_backend_cert" "machine-id" {
   backend     = var.pki_mount_path
   name        = var.pki_role
   common_name = var.common_name
+  ip_sans     = [
+    "127.0.0.1"
+  ]
 }
